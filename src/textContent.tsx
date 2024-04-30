7019c9cd5cd544c42a6ce82761a351a63f9b7a7b
ioddgamers <toluloperbrts@gmail.com>
2024-04-30 09:19:53 +0100

    created TextContent component

---
+++ src/textContent.tsx
@@ -0,0 +1,14 @@
+export const TextContent = () => {
+  return (
+    <div>
+      <h1>Stay Updated</h1>
+      <p> Join 60,000+ product managers receiving monthly updates on:</p>
+
+      <ul>
+        <li>Product discovery and building what matters</li>
+        <li>Measuring to ensure updates are a success</li>
+        <li>And much more!</li>
+      </ul>
+    </div>
+  );
+};
