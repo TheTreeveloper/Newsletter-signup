606de11ade1d959506fcf8a0e29667b525fca2e7
ioddgamers <toluloperbrts@gmail.com>
2024-04-30 14:30:18 +0100

    imported form validation component from material ui

---
+++ src/form.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function FormValidation() {
+  return (
+    <div>
+      <TextField
+        error
+        id="outlined-error"
+        label="Error"
+        defaultValue="Hello World"
+      />
+    </div>
+  );
+}
